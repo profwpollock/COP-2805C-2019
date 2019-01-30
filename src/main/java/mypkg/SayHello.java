f7fd484df34e2ecde0a9bcbc610fd0f9adf8b8dd
Kyle Edward Young <kyleedwardyoung@gmail.com>
2019-01-30 11:26:44 +0200

    Kyle Edward Young commit to return comments

--- src/main/java/mypkg/SayHello.java
+++ src/main/java/mypkg/SayHello.java
@@ -35,6 +35,10 @@ public class SayHello
         System.out.println( "Wow! I am late! Kyle Edward Young my name" );
         counter++;
 
+        // (1) Add your own output below these comments that includes your name.
+        // (2) Place "counter++;" command after your contribution, to keep count.
+        // (3) Keep the existing println statement at the very end.
+
         System.out.println( "Number of students who edited this file: "
             + counter );
     }
