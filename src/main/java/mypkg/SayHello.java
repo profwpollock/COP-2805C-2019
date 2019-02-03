2e955e90b07d202cb2de263b10bb2cc2fcd09dee
Rootbeerener <jakebabb17@gmail.com>
2019-02-02 21:13:24 -0600

    this is my commit message for writing my line of code I wrote code that displays my name and adds to the counter

--- src/main/java/mypkg/SayHello.java
+++ src/main/java/mypkg/SayHello.java
@@ -1,3 +1,68 @@
+<<<<<<< HEAD
+// Git collaborative COP-2805C (Java II) project.
+// $Id$
+
+package mypkg;
+
+/** Class used for COP-2805C git and GitHub project.
+*/
+public class sayHello
+{
+    /** Displays greetings for all students.
+      * When done, displays a count of students who edited this file.
+      * @param args - Unused
+      */
+    public static void main ( String [] args )
+    {
+        int counter = 0;
+
+        System.out.println( "Howdy!  My name is Wayne Pollock." );
+        counter++;
+        
+        System.out.println( "Hello, Hallo, Hullo. This is Kritsia." );
+        counter++;
+
+        System.out.println( "Hello all, My name is Joshua Ventura!" );
+        System.out.println( "A man counted 97 chicken in his chicken coop, but when he rounded them up, he had 100." );
+        counter++;
+
+        System.out.println( "Alex's hint for the test: Pollock's apparent favorite color is Blue." );
+        counter++;
+        
+        System.out.println( "Uh, did I do this correctly? - Michael Tarallo" );
+        counter++;
+        
+        System.out.println("Vegeta! What does the scouter say about his power"
+                + " level? IT'S OVER 9000! DragonBall is dope af - Cody.");
+        counter++;
+
+
+        System.out.println( "Wow! I am late! Kyle Edward Young my name" );
+        counter++;
+
+        System.out.println("Hello everyone, My name is David Alvarez");
+        counter++;
+
+
+
+
+
+
+
+
+
+        System.out.println("ello this is Jacob Babb aperture science would"
+                +"like to remind that cake and grief counsuling will be at the served at the end of the test");
+        counter++;
+        // (1) Add your own output below these comments that includes your name.
+        // (2) Place "counter++;" command after your contribution, to keep count.
+        // (3) Keep the existing println statement at the very end.
+
+        System.out.println( "Number of students who edited this file: "
+            + counter );
+    }
+}
+=======
 // Git collaborative COP-2805C (Java II) project.
 // $Id$
 
@@ -55,13 +120,18 @@ public class SayHello
         counter++;
 
 
+
         // (1) Add your own output below these comments that includes your name.
         // (2) Place "counter++;" command after your contribution, to keep count.
         // (3) Keep the existing println statement at the very end.
         
         System.out.println("Hello! My name is Agnes B.");
+
+        System.out.println("hello my name is jacob Babb, jimmy johns has best bread");
+        counter++;
         
         System.out.println( "Number of students who edited this file: "
             + counter );
     }
 }
+>>>>>>> origin/master
