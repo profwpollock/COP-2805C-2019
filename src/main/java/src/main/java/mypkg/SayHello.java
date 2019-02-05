e76dd1586cbf6eaa8e3a3672aa725304e96cea2e
jasysak <jsysak@hawkmail.hccfl.edu>
2019-02-05 11:39:29 -0500

    Added my name and increment counter

--- src/main/java/src/main/java/mypkg/SayHello.java
+++ src/main/java/src/main/java/mypkg/SayHello.java
@@ -136,6 +136,9 @@ public class SayHello
         System.out.println("Hi, this is Sean Thompson.");
         counter++;
         
+        System.out.println("Hello everyone, my name is Jason Sysak.");
+        counter++;
+        
         System.out.println( "Number of students who edited this file: "
             + counter );
     }
