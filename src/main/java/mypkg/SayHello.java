0388d5989d7d0e2dc39c6127ebf2dc12b9139410
profwpollock <pollock@acm.org>
2019-02-07 13:54:34 -0500

    Removed duplicate entries

--- src/main/java/mypkg/SayHello.java
+++ src/main/java/mypkg/SayHello.java
@@ -32,9 +32,6 @@ public class SayHello
         System.out.println( "Alex's hint for the test: Pollock's apparent favorite color is Blue." );
         counter++;
         
-        System.out.println( "Uh, did I do this correctly? - Michael Tarallo" );
-        counter++;
-        
         System.out.println("Vegeta! What does the scouter say about his power"
                 + " level? IT'S OVER 9000! DragonBall is dope af - Cody.");
         counter++;
@@ -52,33 +49,11 @@ public class SayHello
 
         System.out.println("ello this is Jacob Babb aperture science would"
                 +"like to remind that cake and grief counseling will be at the served at the end of the test");
-        counter++;
-    
-        System.out.println( "Howdy!  My name is Wayne Pollock." );
-        counter++;
-        
-        System.out.println( "Hello, Hallo, Hullo. This is Kritsia." );
-        counter++;
-
-        System.out.println( "Hello all, My name is Joshua Ventura!" );
-        System.out.println( "A man counted 97 chicken in his chicken coop, but when he rounded them up, he had 100." );
-        counter++;
-
-        System.out.println( "Alex's hint for the test: Pollock's apparent favorite color is Blue." );
+        System.out.println("hello my name is jacob Babb, jimmy johns has best bread");
         counter++;
         
         System.out.println( "Uh, did I do this correctly? - Michael Tarallo" );
         System.out.println(" Also, did you know taller people sleep longer in bed?");
-        counter++;
-        
-        System.out.println("Vegeta! What does the scouter say about his power"
-                + " level? IT'S OVER 9000! DragonBall is dope af - Cody.");
-        counter++;
-
-        System.out.println( "Wow! I am late! Kyle Edward Young my name" );
-        counter++;
-
-        System.out.println("Hello everyone, My name is David Alvarez");
         counter++;
 
         System.out.println("Why do Java programmers have to wear glasses?" +
@@ -91,17 +66,9 @@ public class SayHello
         System.out.println( "Hello, Rungrut is here" );
         System.out.println( "Alcohol is a perfect solvent: It dissolves marriages, families and careers." );
         counter++;
-
-        System.out.println( "I am Jeffrey Taylor" );
-        System.out.println( "Why did the programmer quit his job?" );
-        System.out.println( "Because he didn't get arrays" );
-        counter++;
         
         System.out.println("Hello! My name is Agnes B.");
 
-        System.out.println("hello my name is jacob Babb, jimmy johns has best bread");
-        counter++;
-        
         System.out.println("I made some big changes, and I hope it lets this work again. Sam Graham");
         counter++;
         
@@ -137,8 +104,7 @@ public class SayHello
 
         System.out.println( "Mary Sotomayor will definitely require grief counseling.");
         counter++;
-        
-        
+
         System.out.println("Hello, this is Erin Hochstetler.");
         counter++;
         
